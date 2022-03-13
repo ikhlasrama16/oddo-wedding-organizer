d38523ec5d2e557b3fa268b4966d1efec04d853a
muhamadhashmicro <muhamad.hashmicro@gmail.com>
2022-03-14 00:36:18 +0700

    oddo wedding organizer 7 mar

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import models
+from . import partner
+from . import panggung
+from . import pelaminan
+from . import kursipengantin
+from . import kursitamu
+from . import order
---
+++ models/panggung.py
@@ -0,0 +1,46 @@
+from odoo import api, fields, models
+
+
+class Panggung(models.Model):
+    _name = 'wedding.panggung'
+    _description = 'New Description'
+
+    name = fields.Char(string='Name', required=True)
+    pelaminan_id = fields.Many2one(comodel_name='wedding.pelaminan', 
+                                string='TIpe Pelaminan', 
+                                required=True,
+                                domain="[('harga','>','900000')]")
+    kursipengantin_id = fields.Many2one(comodel_name='wedding.kursipengantin',
+                                string='Kursi Pengantin',
+                                required=True)
+    bunga = fields.Selection(string='Tipe Bunga', selection=[('bunga mati', 'Bunga Mati'), ('bunga hidup', 'Bunga Hidup')])
+    accesories = fields.Char(string='Accecories Pelaminan')
+    orderdetail_ids = fields.One2many(
+        comodel_name='wedding.order_detail', 
+        inverse_name='panggung_id', 
+        string='Order Detail')
+    harga = fields.Char(compute='_compute_harga', string='Harga')
+    
+    @api.depends('pelaminan_id', 'kursipengantin_id')
+    def _compute_harga(self):
+        for record in self:
+            record.harga = record.pelaminan_id.harga + record.kursipengantin_id.harga
+
+    stok = fields.Integer(string='Stok Paket Panggung')
+    
+
+    des_pelaminan = fields.Char(compute='_compute_des_pelaminan', string='Deskripsi Pelaminan')
+    
+    @api.depends('pelaminan_id')
+    def _compute_des_pelaminan(self):
+        for record in self:
+            record.des_pelaminan=record.pelaminan_id.deskripsi
+
+
+    
+    des_kursipengantin = fields.Char(compute='_compute_des_kursipengantin', string='Deskripsi Kursi Pengantin')
+    
+    @api.depends('kursipengantin_id')
+    def _compute_des_kursipengantin(self):
+        for record in self:
+            record.des_kursipengantin = record.kursipengantin_id.deskripsi
