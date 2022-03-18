71da8721c98dcee7d7184f8be0327a80e719d153
muhamadhashmicro <muhamad.hashmicro@gmail.com>
2022-03-18 12:49:23 +0700

    oddo wedding organizer 8 mar

--- models/order.py
+++ models/order.py
@@ -1,6 +1,3 @@
-from ast import Store
-from dataclasses import field
-from email.policy import default
 from odoo import api, fields, models
 
 
@@ -8,56 +5,82 @@ class Order(models.Model):
     _name = 'wedding.order'
     _description = 'New Description'
 
-    order_detail_ids = fields.One2many(
-        comodel_name='wedding.order_detail', 
+    orderpanggungdetail_ids = fields.One2many(
+        comodel_name='wedding.orderpanggungdetail', 
         inverse_name='order_id', 
         string='Order Detail')
+    
+    orderkursitamudetail_ids = fields.One2many(
+        comodel_name='wedding.orderkursitamudetail', 
+        inverse_name='orderk_id', 
+        string='Order Kursi Tamu')
+    
+    
     name = fields.Char(string='Kode Order', required=True)
-    tanggal_pesan = fields.Date('Tanggal Pemesanan', default=fields.Datetime.now())
-    total = fields.Integer(compute='_compute_total', string='Total', Store=True)
-
-    @api.depends('order_detail_ids')
+    tanggal_pesan = fields.Datetime('Tanggal Pemesanan',default=fields.Datetime.now())
+    tanggal_pengiriman = fields.Date(string='Tanggal Pengiriman', default=fields.Date.today())
+    
+    total = fields.Integer(compute='_compute_total', string='Total', store=True)
+    
+    @api.depends('orderpanggungdetail_ids')
     def _compute_total(self):
         for record in self:
-            a = sum(self.env['wedding.order_detail'].search([('order_id','=', record.id)]).mapped('harga'))
-            record.total = a
+            a = sum(self.env['wedding.orderpanggungdetail'].search([('order_id', '=', record.id)]).mapped('harga'))
+            b = sum(self.env['wedding.orderkursitamudetail'].search([('orderk_id', '=', record.id)]).mapped('harga'))
+            record.total = a + b
     
-    
 
-
-
-class OrderDetail(models.Model):
-    _name = 'wedding.order_detail'
+class OrderPanggungDetail(models.Model):
+    _name = 'wedding.orderpanggungdetail'
     _description = 'New Description'
 
     order_id = fields.Many2one(comodel_name='wedding.order', string='Order')
-    panggung_id = fields.Many2one(comodel_name='wedding.panggung', string='Panggung')
-    name = fields.Selection(string='Name', selection=[('panggung', 'Panggung'), ('kurs itamu', 'Kursi Tamu'),])
+    panggung_id = fields.Many2one(comodel_name='wedding.panggung', string='Panggung')   
+    
+         
+    name = fields.Char(string='Name')
     harga = fields.Integer(compute='_compute_harga', string='harga')
     qty = fields.Integer(string='Quantity')
-
-    harga_satuan = fields.Integer(compute='_compute_harga_satuan', string='Harga Satuan')
+    harga_satuan = fields.Integer(compute='_compute_harga_satuan', string='harga_satuan')
     
     @api.depends('panggung_id')
     def _compute_harga_satuan(self):
         for record in self:
             record.harga_satuan = record.panggung_id.harga
     
-
+    
     @api.depends('qty','harga_satuan')
     def _compute_harga(self):
         for record in self:
-            record.harga = record.harga_satuan * record.qty
-    
-
+           record.harga = record.harga_satuan * record.qty
+           
     @api.model
-    def create(self, vals):
-        record = super(OrderDetail,self).create(vals)
+    def create(self,vals):
+        record = super(OrderPanggungDetail, self).create(vals) 
         if record.qty:
-            self.env['wedding.panggung'].search([('id', '=', record.panggung_id.id)]).write({'stok':record.panggung_id.stok-record.qty})
+            self.env['wedding.panggung'].search([('id','=',record.panggung_id.id)]).write({'stok':record.panggung_id.stok-record.qty})
             return record
-    
         
+class OrderKursiTamuDetail(models.Model):
+    _name = 'wedding.orderkursitamudetail'
+    _description = 'New Description'
     
+    orderk_id = fields.Many2one(comodel_name='wedding.order', string='Order Kursi')
+    kursitamu_id = fields.Many2one(comodel_name='wedding.kursitamu', string='Kursi Tamu')
     
-
+    name = fields.Char(string='Name')
+    harga_satuan = fields.Integer(compute='_compute_harga_satuan', string='harga_satuan')
+    
+    @api.depends('kursitamu_id')
+    def _compute_harga_satuan(self):
+        for record in self:
+            record.harga_satuan = record.kursitamu_id.harga
+    
+    qty = fields.Integer(string='Quantity')
+    
+    harga = fields.Integer(compute='_compute_harga', string='harga')
+    
+    @api.depends('harga_satuan','qty')
+    def _compute_harga(self):
+        for record in self:
+               record.harga = record.harga_satuan * record.qty
--- models/panggung.py
+++ models/panggung.py
@@ -7,36 +7,31 @@ class Panggung(models.Model):
 
     name = fields.Char(string='Name', required=True)
     pelaminan_id = fields.Many2one(comodel_name='wedding.pelaminan', 
-                                string='TIpe Pelaminan', 
-                                required=True,
-                                domain="[('harga','>','900000')]")
-    kursipengantin_id = fields.Many2one(comodel_name='wedding.kursipengantin',
-                                string='Kursi Pengantin',
-                                required=True)
-    bunga = fields.Selection(string='Tipe Bunga', selection=[('bunga mati', 'Bunga Mati'), ('bunga hidup', 'Bunga Hidup')])
-    accesories = fields.Char(string='Accecories Pelaminan')
-    orderdetail_ids = fields.One2many(
-        comodel_name='wedding.order_detail', 
-        inverse_name='panggung_id', 
-        string='Order Detail')
-    harga = fields.Char(compute='_compute_harga', string='Harga')
+                                string='Tipe Pelaminan', 
+                                required=True)   
+    kursipengantin_id = fields.Many2one(comodel_name='wedding.kursipengantin', 
+                                        string='Kursi Pengantin', 
+                                        required=True)
     
-    @api.depends('pelaminan_id', 'kursipengantin_id')
+    bunga = fields.Selection(string='Tipe Bunga', selection=[('bunga mati', 'Bunga Dead'), ('bunga hidup', 'Bunga Life')])    
+    accesories = fields.Char(string='Accesories Pelaminan')
+    
+    
+    harga = fields.Integer(compute='_compute_harga', string='Harga')
+    
+    @api.depends('pelaminan_id','kursipengantin_id')
     def _compute_harga(self):
         for record in self:
             record.harga = record.pelaminan_id.harga + record.kursipengantin_id.harga
-
+    
     stok = fields.Integer(string='Stok Paket Panggung')
     
-
     des_pelaminan = fields.Char(compute='_compute_des_pelaminan', string='Deskripsi Pelaminan')
     
     @api.depends('pelaminan_id')
     def _compute_des_pelaminan(self):
         for record in self:
-            record.des_pelaminan=record.pelaminan_id.deskripsi
-
-
+            record.des_pelaminan = record.pelaminan_id.deskripsi
     
     des_kursipengantin = fields.Char(compute='_compute_des_kursipengantin', string='Deskripsi Kursi Pengantin')
     
